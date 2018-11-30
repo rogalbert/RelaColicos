535a1aa7d52aa218219df6c3137e10ed8a416ad3
achandec <achandec@villanova.edu>
2018-11-29 19:09:38 -0500

    Initialization of both teams flags on either side of the field in the random locations

--- flag_init.m
+++ flag_init.m
@@ -1,15 +1,23 @@
 %% Flag_init 
 % Initializes the positions of the flags
 
-function [L,R] = flag_init()
+function [Left_handle,Right_handle] = flag_init()
     left_flag_pos = rand([2,1]);
-    left_flag_pos(1) = left_flag_pos(1)*2 - 1
-    left_flag_pos(2) = left_flag_pos(2)*1.6 - 1.6
+    left_flag_pos(1) = left_flag_pos(1)*(-1.5); 
+    left_flag_pos(2) = left_flag_pos(2)*1.9 - 1;
+    
     
     right_flag_pos = rand([2,1]);
-    right_flag_pos(1) = right_flag_pos(1)*2 - 1
-    right_flag_pos(2) = right_flag_pos(2)*1.6
+    right_flag_pos(1) = right_flag_pos(1)*1.5;
+    right_flag_pos(2) = right_flag_pos(2)*1.9 - 1;
     
-    L = left_flag_pos;
-    R = right_flag_pos;
+    
+    % Creating Plot handles
+    Left_handle = cell(1);
+    Right_handle = cell(1);
+    
+    Left_handle = plot(left_flag_pos(1),left_flag_pos(2),'r.');
+    Right_handle = plot(right_flag_pos(1), right_flag_pos(2),'b.');
+    
+    
 end
